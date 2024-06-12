da0ef465343ea0c9e0cf613a04d3422c703313a1
Joao Iguana <jiboiadamor@gmail.com>
2024-06-12 18:57:13 +0100

    added root route for welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "static#welcome"
+
   resources :tasks
   resources :buckets
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
